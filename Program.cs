2a1c11c2972e99cae118ecbee43c68a806e2f280
uhorin <144419399+uhorin@users.noreply.github.com>
2024-03-15 10:52:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Створіть клас ArrayManipulator, який має методи для роботи з масивами цілих чисел:
+//Метод GenerateRandomArray(int length, int min, int max), який створює та повертає новий масив заданої довжини з випадковими числами в діапазоні від min до max.
+//Метод FindMax(int[] array), який знаходить та повертає найбільший елемент у масиві.
+//Метод SortArray(int[] array), який сортує масив у зростаючому порядку.
+//Після створення класу запустіть програму, яка створює масив, знаходить найбільший елемент та сортує масив. Виведіть початковий масив, знайдений максимум та відсортований масив на консоль.
+using System;
+
+class ArrayManipulator
+{
+    int[] array;
+    public int FindMax(int[] array)
+    {
+        int max = 0;
+        for (int i =0; i<array.Length; i++)
+        {
+            if (array[i]>max) {
+                max=array[i];
+            }
+        }
+        return max;
+    }
+    
+  
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayManipulator array = new ArrayManipulator();
+
+    }
+}
